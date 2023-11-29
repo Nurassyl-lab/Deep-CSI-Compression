8ba441e92cb47d800f70abeeba1ad54af369cd55
Nurassyl-lab <nurassyl.askar@gmail.com>
2023-11-29 14:26:02 +0800

    adding someing

--- code/main.py
+++ code/main.py
@@ -10,9 +10,13 @@ import argparse
 # using argparse get the args
 parser = argparse.ArgumentParser(description='Run the test')
 parser.add_argument('--test', type=str, default='test1', help='choose the test you want to run')
+
 args = parser.parse_args()
 
 
 
 if __name__ == '__main__':
     print(f'Running the test --> {args.test}')
+
+import tensorflow as tf
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
